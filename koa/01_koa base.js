3d29f5760358b0da5f1b5f06f35b94e2e0b8b034
itchaox <1462831573@qq.com>
2023-09-06 22:40:38 +0800

    feat(koa): 文件上传

--- koa/01_koa base.js
+++ koa/01_koa base.js
@@ -2,40 +2,49 @@
  * @Version    : v1.00
  * @Author     : wangchao
  * @Date       : 2023-09-06 16:43
- * @LastAuthor : wangchao
- * @LastTime   : 2023-09-06 18:17
+ * @LastAuthor : itchaox
+ * @LastTime   : 2023-09-06 22:33
  * @desc       :
  */
-const Koa = require("koa");
-const KoaRouter = require("@koa/router");
-
-const bodyParser = require("koa-bodyparser");
+const Koa = require('koa');
+const KoaRouter = require('@koa/router');
+const multer = require('@koa/multer');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
+const formParser = multer({
+  dest: './imgs',
+});
 
 app.use(bodyParser());
 
-const userRouter = new KoaRouter({ prefix: "/users" });
+const userRouter = new KoaRouter({ prefix: '/users' });
 
 // get query
-userRouter.get("/login", (ctx, next) => {
-  ctx.body = "userRouter login 成功" + JSON.stringify(ctx.query);
+userRouter.get('/login', (ctx, next) => {
+  ctx.body = 'userRouter login 成功' + JSON.stringify(ctx.query);
 });
 
 // get params /:id
-userRouter.get("/login/:id", (ctx, next) => {
+userRouter.get('/login/:id', (ctx, next) => {
   const id = ctx.params.id;
 
-  ctx.body = "您输入的id为：" + id;
+  ctx.body = '您输入的id为：' + id;
 });
 
 // post json
-userRouter.post("/login", (ctx, next) => {
+userRouter.post('/login', (ctx, next) => {
   console.log(ctx.request.body);
+  ctx.body = '恭喜你!!!';
+});
+
+userRouter.post('/form', formParser.array('img'), (ctx, next) => {
+  console.log(ctx.request.file);
+  ctx.body = '文件上传成功';
 });
 
 app.use(userRouter.routes());
 
-app.listen("8989", () => {
-  console.log("6666");
+app.listen('8989', () => {
+  console.log('6666');
 });
